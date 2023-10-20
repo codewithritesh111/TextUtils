9e7014f11a2396ac6d4c1e763ee3f992ed0a0260
codewithritesh111 <139766751+codewithritesh111@users.noreply.github.com>
2023-10-20 11:00:27 +0530

    Add files via upload

---
+++ module1.mjs
@@ -0,0 +1,8 @@
+let a = "Ritesh"
+let b = "Sakshi"
+let c = "Krishna"
+let d = "Karn"
+
+export default a;
+export {b}
+export {c}
---
+++ module2.mjs
@@ -0,0 +1,4 @@
+import uk,{b,c} from "./module1.mjs"
+console.log(uk)
+console.log(b)
+console.log(c)
